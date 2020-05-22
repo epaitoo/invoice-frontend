be2cf44fe1a961ec39ee92072efdff04c91005e7
Eugene Paitoo <eugenepaitoo@gmail.com>
2020-05-22 21:08:21 +0000

    add page loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,18 @@
 import '../public/assets/css/style.bundle.css'
 import '../public/assets/plugins/global/plugins.bundle.css'
 import 'react-toastify/dist/ReactToastify.css';
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
 
 // Custom CSS for a page
-import '../public/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css'
-import  '../public/assets/css/pages/login/login-2.css'
+import '../public/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css';
+import  '../public/assets/css/pages/login/login-2.css';
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 
 
