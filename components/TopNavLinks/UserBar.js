7f3fafde9935da148cd76d00749857c7b1092a20
Eugene Paitoo <eugenepaitoo@gmail.com>
2021-05-03 16:31:17 +0000

    get username function

--- components/TopNavLinks/UserBar.js
+++ components/TopNavLinks/UserBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link'
-import { getUserName } from '../../services/Helper'
+// import { getUserName } from '../../services/Helper'
 
 
 const bgImg = {
